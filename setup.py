3a012ba4a7f9ac08798a20b9a216fd902cabbb95
Ashish Kumar <ak78774@gmail.com>
2025-02-20 23:48:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="p4print",  
-    version="0.2.1", 
+    name="p4print",
+    version="0.1",
     packages=find_packages(),
-    description="A simple package to use 'p()' instead of 'print()'",
-    long_description=open("README.md").read(),
+    description="Use 'p()' instead of 'print()' in Python",
+    long_description=open("README.md", encoding="utf-8").read(),  # Ensure README.md exists
     long_description_content_type="text/markdown",
     author="Ashish Kumar",
     author_email="ashish.jnu@outlook.com",
-    url="https://github.com/AshishKumarCS/p4print",  
+    url="https://github.com/AshishKumarCS/p4print",
     license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.0",
+    python_requires=">=3.6",
 )
