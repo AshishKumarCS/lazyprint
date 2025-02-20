1721d32c3cab6629979ff72fd3be1a4e679eb065
AshishKumarCS <ak78774@gmail.com>
2025-02-20 22:37:26 +0530

    Lazyprint uploaded

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lazyprint",  # Package name
+    version="0.1",  # Initial version
+    packages=find_packages(),
+    description="I need to test output of programs many times. And, writing print many times make me slow. So, here is a simple package to use 'p()' instead of 'print()'",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Ashish Kumar",
+    author_email="ashish.jnu@outlook.com",
+    url="https://github.com/AshishKumarCS/lazyprint",  # GitHub repo
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.0",
+)
