0ff96a0d75ff5632b80f2a31ded92a8034197005
Ashish Kumar <ak78774@gmail.com>
2025-02-20 23:33:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="p4print",  
-    version="0.2",
+    name="p4print",  # ✅ Change to your new package name
+    version="0.2.1",  # Increment the version to avoid PyPI duplication issues
     packages=find_packages(),
-    description="A simple package to use 'p()' instead of 'print()' for faster coding.",
+    description="A simple package to use 'p()' instead of 'print()'",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Ashish Kumar",
     author_email="ashish.jnu@outlook.com",
-    url="https://github.com/AshishKumarCS/p4print",  
+    url="https://github.com/AshishKumarCS/p4print",  # ✅ Update the GitHub repo link
     license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
